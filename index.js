7942cd6a4256faac4714042f55b2b609871723da
AikoP <aiko.pipo@hotmail.de>
2021-04-03 22:24:08 +0200

    p8443

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
 	res.send('<h1>Welcome to Node.js project setup</h1>')
 })
 
-app.listen(8030, function(){
-	console.log("Server started on port: 8030")
+app.listen(8443, function(){
+	console.log("Server started on port: 8443")
 })
